90cb0a023d6caa8dd69cc54dd673ef0f1bdd4bd3
Chris Roy <cmr223@miami.edu>
2015-11-16 13:43:39 -0500

    added p5 play animation

---
+++ js/animation.js
@@ -0,0 +1,23 @@
+var wiz;
+
+function preload() {
+
+//create an animation from a sequence of numbered images
+//pass the first and the last file name and it will try to find the ones in between
+wiz = loadAnimation("img_files/WizStand_00.png", "img_files/WizStand_20.png");
+  
+}
+
+function setup() {
+  var myCanvas = createCanvas(362, 487);
+	myCanvas.parent('walker');
+}
+
+function draw() {
+  background(255,255,255);  
+  
+  //specify the animation instance and its x,y position
+  //animation() will update the animation frame as well
+  animation(wiz, 362/2, 487/2);
+}
+
--- js/app.js
+++ js/app.js
@@ -5,25 +5,23 @@ $('#result').hide();
 $('#notify').hide();
 
 
-var totalFrames = 18;
-var frameWidth = 174;
-var speed = 1.5;
-  
-var walkEase = new SteppedEase(totalFrames)
-var finalPosition = '-' + (frameWidth * totalFrames) + 'px 0px';
-
-var walkTL = new TimelineMax()
-walkTL.to('#walker', speed, {
-    backgroundPosition: finalPosition,
-    ease: walkEase,
-    repeat: -1
-});
+//var totalFrames = 18;
+//var frameWidth = 174;
+//var speed = 1.5;
+//  
+//var walkEase = new SteppedEase(totalFrames)
+//var finalPosition = '-' + (frameWidth * totalFrames) + 'px 0px';
+//
+//var walkTL = new TimelineMax()
+//walkTL.to('#walker', speed, {
+//    backgroundPosition: finalPosition,
+//    ease: walkEase,
+//    repeat: -1
+//});
 
 var description;
 var aqi; // integer
 var color;
-
-
 
 function showWeather() {
     console.log("weather");
